bdb03630f6170f93ef26c94929a42f2873a724cc
Harsh Mittal <harshmittal1750@gmail.com>
2023-12-18 00:12:25 +0530

    removed redux

--- frontend/src/redux/provider.tsx
+++
@@ -1,8 +0,0 @@
-// "use client";
-
-// import { Provider } from "react-redux";
-// import store from "./store";
-// import React, { ReactNode } from "react";
-// export function ReduxProvider({ children }: { children: ReactNode }) {
-//   return <Provider store={store}>{children}</Provider>;
-// }
