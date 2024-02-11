1ce83fefc59647c10e0a5d613b5a97a5ba6b87de
Harsh Mittal <harshmittal1750@gmail.com>
2024-02-11 10:45:40 +0530

    Refactor QuestionsProps interface

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -15,16 +15,16 @@ export interface MarketProps {
   endTimestamp?: any;
   resolverUrl?: any;
 }
-// export interface QuestionsProps {
-//   id: any;
-//   title?: any;
-//   imageHash?: any;
-//   totalAmount?: any;
-//   totalYes?: any;
-//   totalNo?: any;
-//   hasResolved?: boolean;
-//   endTimestamp?: any;
-// }
+export interface QuestionsProps {
+  id: any;
+  title?: any;
+  imageHash?: any;
+  totalAmount?: any;
+  totalYes?: any;
+  totalNo?: any;
+  hasResolved?: boolean;
+  endTimestamp?: any;
+}
 
 // export interface MarketProps {
 //     id: any;
