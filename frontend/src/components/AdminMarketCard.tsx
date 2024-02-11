8133343a5dde60ecb76333cb1564ec159c957771
Harsh Mittal <harshmittal1750@gmail.com>
2024-02-11 10:43:37 +0530

    Resolve markets updated ui

--- frontend/src/components/AdminMarketCard.tsx
+++ frontend/src/components/AdminMarketCard.tsx
@@ -1,15 +1,14 @@
 "use client";
+import React from "react";
 import {
   Box,
   Typography,
   Button,
-  Avatar,
   Card,
   CardContent,
   CardActions,
+  CardMedia,
 } from "@mui/material";
-import Image from "next/image";
-import React from "react";
 import Web3 from "web3";
 
 interface Props {
@@ -23,84 +22,64 @@ interface Props {
 
 export const AdminMarketCard: React.FC<Props> = ({
   title,
+  imageHash,
   totalAmount,
   onYes,
   onNo,
-  imageHash,
 }) => {
+  const ipfsBaseUrl = "https://ipfs.io/ipfs/";
+
+  const formattedImageHash = imageHash ? imageHash.replace("ipfs://", "") : "";
+
+  const imageUrl = formattedImageHash
+    ? `${ipfsBaseUrl}${formattedImageHash}`
+    : "https://source.unsplash.com/random";
+
   return (
     <Box sx={{ width: "100%", marginBottom: 2 }}>
       <Card
-        sx={{ cursor: "pointer", "&:hover": { boxShadow: 3 }, borderRadius: 2 }}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          cursor: "pointer",
+          "&:hover": { boxShadow: 3 },
+          borderRadius: 2,
+          overflow: "hidden",
+        }}
       >
+        <CardMedia
+          component="img"
+          image={imageUrl}
+          alt="Market"
+          sx={{ height: 140, objectFit: "cover" }} // Adjust height as needed
+        />
         <CardContent>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 2,
-              alignItems: "center",
-              paddingBottom: 2,
-            }}
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ fontWeight: "bold", marginBottom: 2 }}
           >
-            <Avatar
-              // src={`https://ipfs.infura.io/ipfs/${market?.imageHash}`}
-              src={"/"}
-              sx={{ width: 55, height: 55 }}
-              alt="Image Hash"
-            />
-            <Typography
-              variant="h6"
-              component="span"
-              sx={{ fontWeight: "bold" }}
-            >
-              {title}
+            {title}
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography variant="body2" color="text.secondary">
+              Total Liquidity:{" "}
+              {totalAmount
+                ? `${parseFloat(
+                    Web3.utils.fromWei(totalAmount, "ether")
+                  ).toFixed(2)} PARA`
+                : "0 PARA"}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Ending In: 12 Days
             </Typography>
           </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-              <Typography variant="caption" color="text.secondary">
-                Total Liquidity
-              </Typography>
-              <Typography variant="body1">
-                {totalAmount &&
-                  parseFloat(Web3.utils.fromWei(totalAmount, "ether")).toFixed(
-                    2
-                  )}{" "}
-                PARA
-              </Typography>
-            </Box>
-            <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-              <Typography variant="caption" color="text.secondary">
-                Ending In
-              </Typography>
-              <Typography variant="body1">12 Days</Typography>
-            </Box>
-          </Box>
         </CardContent>
-        <CardActions
-          sx={{ display: "flex", justifyContent: "flex-end", gap: 1 }}
-        >
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={onYes}
-            sx={{ textTransform: "none" }}
-          >
+        <CardActions sx={{ justifyContent: "flex-end" }}>
+          <Button variant="outlined" onClick={onYes} sx={{ marginRight: 1 }}>
             Resolve YES
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={onNo}
-            sx={{ textTransform: "none" }}
-          >
+          <Button variant="outlined" color="secondary" onClick={onNo}>
             Resolve NO
           </Button>
         </CardActions>
